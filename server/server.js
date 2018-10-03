71fa5f44142408b06814fd74f46289d9afc6ce89
unknown <suman.logon@gmail.com>
2018-10-03 14:11:20 -0500

    'socket'

--- server/server.js
+++ server/server.js
@@ -3,14 +3,36 @@
 
 const path=require('path');
 
+const express=require('express');
+
+const socketIO=require('socket.io');
+
+const http = require('http');
+
 const publicPath=path.join(__dirname,'../public');
 
-const express=require('express');
+const port=process.env.PORT || 8080
 
 var app=express();
 
+var server=http.createServer(app);
+
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(3000,()=>{
-  console.log('Server is up and running');
+io.on('connection',(socket)=>{
+
+console.log('New User Connected');
+
+socket.on('disconnect',()=>{
+  console.log('User was disconnected');
+});
+
+});
+
+
+
+server.listen(port,()=>{
+  console.log(`server is up and running in ${port}`);
 });
